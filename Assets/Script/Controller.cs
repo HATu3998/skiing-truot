891e72f2b7cfa5ceea342e995ac736e42153f09e
Tus <3artemis98@gmail.com>
2025-06-27 07:13:46 +0700

    truot rightleft

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class Controller : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float torque = 1;
+    [SerializeField] Rigidbody2D rb;
+   // Vector3 originalScale;
+    void Start()
+    {
+     //   originalScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb == null)
+        {
+            Debug.LogError("Chưa gán Rigidbody2D cho Controller!");
+            return;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddTorque(torque);
+           // transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddTorque(-torque);
+           // transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
+    }
+}
---
+++ Assets/Script/Crash.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Crash : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col )
+    {
+        if(col.tag == "Ground")
+        {
+            Debug.Log("da cham vao dau");
+        }
+    }
+}
